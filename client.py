c14cfb737abf5b95fe47ec63ab0e4be73a3d6bad
Niv Berlin <127884223+NVB20@users.noreply.github.com>
2025-05-04 23:08:18 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,16 @@
+from psycopg2 import connect
+import os
+
+def _get_postgres_connection():
+    return connect(
+        dbname=os.getenv("POSTGRES_DB", "postgres"),
+        user=os.getenv("POSTGRES_USER", "postgres"),
+        password=os.getenv("POSTGRES_PASSWORD", "password"),
+        host=os.getenv("POSTGRES_HOST", "db"),  
+        port=os.getenv("POSTGRES_PORT", "5432"),
+    )
+
+def get_db_client(db_type: str):
+    if db_type == "postgres":
+        return _get_postgres_connection()
+    raise ValueError(f"Unsupported DB type: {db_type}")
